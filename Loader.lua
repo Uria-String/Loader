aa92e4e7644e0cc4ddcd1582149a316aebf9410a
Uria-Trash <113226115+Uria-Trash@users.noreply.github.com>
2023-02-09 20:52:53 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Masurix/Uria/main/"..({
+  [994732206] = "BloxFurit",  
+  [2788229376] = "DaHood",
+})[game.GameId]))()
